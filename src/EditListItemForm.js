5367bf2fc061efebad6686bc3a04a8212a42cb98
acer-mono <margaritastar92@gmail.com>
2021-02-12 21:54:09 +0700

    fix blur

---
+++ src/EditListItemForm.js
@@ -0,0 +1,39 @@
+import React, { useState, useRef } from 'react';
+
+export default function EditListItem({ item, editName, closeItem }) {
+  const [name, setName] = useState(item.name);
+  const button = useRef(null);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    editName(item.id, name);
+    closeItem();
+  }
+
+  function blurHandler(e) {
+    if (e.relatedTarget === button.current) {
+      editName(item.id, name);
+      closeItem();
+    } else {
+      closeItem();
+    }
+  }
+
+  return (
+    <div>
+      <form action="" onSubmit={e => submitHandler(e)}>
+        <input
+          type="text"
+          value={name}
+          onChange={e => {
+            setName(e.target.value);
+          }}
+          onBlur={e => blurHandler(e)}
+        />
+        <button ref={button} type="submit">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,42 +1,15 @@
 import React, { useState } from 'react';
+import EditListItem from './EditListItemForm';
 
 export default function ListItem({ item, removeHandler, changeState, editName }) {
   const isDone = { textDecoration: 'line-through' };
   const [isEdit, setIsEdit] = useState('');
-  const [name, setName] = useState(item.name);
-  function edit() {
-    setIsEdit(!isEdit);
-  }
+
   return (
     <>
       {!isEdit && <li style={item.isDone ? isDone : {}}>{item.name}</li>}
       {isEdit && (
-        <>
-          <br />
-          <form
-            action=""
-            onSubmit={e => {
-              console.log('submit');
-              e.preventDefault();
-              editName(item.id, name);
-              setIsEdit(!isEdit);
-            }}
-          >
-            <input
-              type="text"
-              value={name}
-              onChange={e => {
-                setName(e.target.value);
-              }}
-              onBlur={() => {
-                console.log('blur');
-                setIsEdit(!isEdit);
-              }}
-            />
-            <button type="submit">Save</button>
-          </form>
-          <br />
-        </>
+        <EditListItem item={item} editName={editName} closeItem={() => setIsEdit(false)} />
       )}
       <input
         type="checkbox"
@@ -44,7 +17,7 @@ export default function ListItem({ item, removeHandler, changeState, editName })
         onChange={() => changeState(item.id, !item.isDone)}
       />
       <button onClick={() => removeHandler(item.id)}>Remove</button>
-      <button onClick={() => edit()}>{!isEdit ? 'Edit' : 'Cancel'}</button>
+      <button onClick={() => setIsEdit(!isEdit)}>{!isEdit ? 'Edit' : 'Cancel'}</button>
     </>
   );
 }
