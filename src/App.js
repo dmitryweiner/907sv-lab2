541bc043d9277fc421457dfa3bd26ce38b8abf3a
dmitryweiner <dmitry.weiner@gmail.com>
2021-02-20 15:05:31 +0700

    Update instructions

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ function App() {
     <div className="wrapper">
       <div>
         <h1>Список дел</h1>
-        <h2>Лабораторная №2. Добавляем элемент в список</h2>
+        <h2>Лабораторная №2. Динамический список</h2>
       </div>
       <div>
         <input type="text" />
         <button>Добавить</button>
         <ul>
-          <li>Купить картошки</li>
-          <li>Помыть пол</li>
-          <li>Покормить рыб</li>
-          <li>Выгулять кошку</li>
+          <li>
+            Купить картошки
+            <button>[x]</button>
+          </li>
+          <li>
+            Помыть пол
+            <button>[x]</button>
+          </li>
+          <li>
+            Покормить рыб
+            <button>[x]</button>
+          </li>
+          <li>
+            Выгулять кошку
+            <button>[x]</button>
+          </li>
         </ul>
       </div>
     </div>
