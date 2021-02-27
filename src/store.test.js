ba1058d5ff0fd621953101fde029d5df42a27951
acer-mono <margaritastar92@gmail.com>
2021-02-27 23:49:12 +0700

    add store tests

---
+++ src/store.test.js
@@ -0,0 +1,103 @@
+import { changePosition } from './store';
+import reducer from './store';
+
+describe('changePosition tests', () => {
+  let items = null;
+  beforeEach(() => {
+    items = [
+      { id: '0', name: 'first', isDone: false, position: 0 },
+      { id: '1', name: 'second', isDone: false, position: 1 },
+      { id: '2', name: 'last', isDone: false, position: 2 }
+    ];
+  });
+
+  test('move up second element to one position', () => {
+    const updatedList = changePosition('1', 1, items);
+    const second = updatedList.find(el => el.id === '1');
+    const first = updatedList.find(el => el.id === '0');
+    expect(first.position).toBe(1);
+    expect(second.position).toBe(0);
+  });
+
+  test('move down second element to one position', () => {
+    const updatedList = changePosition('1', -1, items);
+    const second = updatedList.find(el => el.id === '1');
+    const last = updatedList.find(el => el.id === '2');
+    expect(second.position).toBe(2);
+    expect(last.position).toBe(1);
+  });
+
+  test('move up first element to one position', () => {
+    const updatedList = changePosition('0', 1, items);
+    const second = updatedList.find(el => el.id === '0');
+    expect(second.position).toBe(0);
+  });
+
+  test('move down last element to one position', () => {
+    const updatedList = changePosition('2', -1, items);
+    const second = updatedList.find(el => el.id === '2');
+    expect(second.position).toBe(2);
+  });
+});
+
+describe('reducer test', () => {
+  let items = null;
+  beforeEach(() => {
+    items = [
+      { id: '0', name: 'first', isDone: false, position: 0 },
+      { id: '1', name: 'second', isDone: false, position: 1 },
+      { id: '2', name: 'last', isDone: false, position: 2 }
+    ];
+  });
+
+  test('remove existing item from list', () => {
+    const removeItem = items[0];
+    const action = { name: 'remove', itemId: removeItem.id };
+    const newItems = reducer(action, items);
+    expect(newItems).not.toEqual(items);
+    expect(newItems).not.toContain(removeItem);
+  });
+
+  test('remove not existing item from list', () => {
+    const action = { name: 'remove', itemId: '1000' };
+    const newItems = reducer(action, items);
+    expect(newItems).toEqual(items);
+  });
+
+  test('change position of second item', () => {
+    const changePositionItem = items[1];
+    expect(changePositionItem.position).toBe(1);
+    const action = { name: 'changePosition', itemId: changePositionItem.id, itemNumber: 1 };
+    reducer(action, items);
+    expect(changePositionItem.position).toBe(0);
+  });
+
+  test('change state of item', () => {
+    const changeStateItem = items[1];
+    expect(changeStateItem.isDone).toBe(false);
+    const action = { name: 'changeState', itemId: changeStateItem.id, itemIsDone: true };
+    reducer(action, items);
+    expect(changeStateItem.isDone).not.toBe(false);
+  });
+
+  test('edit item', () => {
+    const editItem = items[1];
+    expect(editItem.name).toBe('second');
+    const action = { name: 'edit', itemId: editItem.id, itemName: 'first' };
+    reducer(action, items);
+    expect(editItem.name).toBe('first');
+  });
+
+  test('create new item', () => {
+    const newItem = { id: '3', name: 'lastLast', isDone: false, position: 3 };
+    const action = { name: 'create', item: newItem };
+    const newItems = reducer(action, items);
+    expect(newItems).toContain(newItem);
+  });
+
+  test('pass wrong action name', () => {
+    const action = { name: 'credcdcdcdate' };
+    const newItems = reducer(action, items);
+    expect(newItems).toEqual(items);
+  });
+});
