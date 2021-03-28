b2689469d58520aa5f0caf33a1fadf7d15f80d67
ninel <zabaykinanm@mer.ci.nsu.ru>
2021-03-28 11:24:02 +0700

    add list test

---
+++ src/components/List.test.js
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import List from './List';
+
+const list = [
+  {
+    id: 1,
+    text: '123'
+  },
+  {
+    id: 2,
+    text: '1234'
+  },
+  {
+    id: 3,
+    text: '123'
+  }
+];
+
+test('correctly displays an empty array', () => {
+  const list = [];
+  const deleteHandler = jest.fn();
+  render(<List list={list} deleteHandler={deleteHandler} />);
+
+  expect(screen.getByText('not elements')).toBeInTheDocument();
+});
+
+test('correctly displays array', () => {
+  const deleteHandler = jest.fn();
+  render(<List list={list} deleteHandler={deleteHandler} />);
+  for (let item of list) {
+    expect(screen.getByText(item.text)).toBeInTheDocument();
+  }
+
+  for (let button of screen.getAllByTestId('delete_button')) {
+    fireEvent.click(button);
+  }
+  expect(deleteHandler).toBeCalledTimes(list.length);
+});
