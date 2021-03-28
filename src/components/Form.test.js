f260bdf5a03dcc2643a61dbf6a23811e1dc2dbcd
ninel <zabaykinanm@mer.ci.nsu.ru>
2021-03-28 11:07:35 +0700

    add form test

---
+++ src/components/Form.test.js
@@ -0,0 +1,14 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Form from './Form';
+
+test('can enter text and submit', () => {
+  const handleSubmit = jest.fn();
+  const field = 'field contents';
+  render(<Form handleSubmit={handleSubmit} />);
+  const input = screen.getByTestId('input');
+  const form = screen.getByTestId('form');
+  fireEvent.input(input, { target: { value: field } }); // ввод в поле
+  expect(handleSubmit).not.toBeCalled();
+  fireEvent.submit(form); // отправка формы
+  expect(handleSubmit).toBeCalledWith(expect.objectContaining({ field }));
+});
